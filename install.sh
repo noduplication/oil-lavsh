f9da9c4d65dde681fd9fae2f65ff56607e9c6979
noduplication <157242043+noduplication@users.noreply.github.com>
2024-01-20 14:21:40 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh.
+
+echo "installing system files..."
+git clone https://github.com/nobuplication/oil-lavsh
+echo "installing github-port0"
+mkdir main
+cd main
+touch info.txt
+touch info.txt >> "FASTBOI-SPINE 1.0.0 2020-2024"
+sudo apt update
